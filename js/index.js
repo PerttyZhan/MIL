83bf51e9d86d60c8ff0e3963d84aacd38803c202
janes <1272486734@qq.com>
2015-09-21 10:56:01 +0800

    zhanheng 2 version

--- js/index.js
+++ js/index.js
@@ -8,7 +8,9 @@
 			this.blog = this.getJson( $('#blog') );
 			this.intro = this.getJson( $('#intro') );
 			this.join = this.getJson( $('#join') );
-
+			this.module = $('*[module="runto"]');
+			this.btnLogin = $('#login');
+			this.closeLoin = $('.icon-close');
 			this.addEvent();
 		},
 		initAnimate:function(){
@@ -18,68 +20,75 @@
 			if( this.header != null && this.intense( this.header,wjson ) ){
 
 				this.header.obj.find('.topbar,.fouse-des,.shequ,.mouse').addClass('animated fadeInDown')
-				this.header.obj.find('.topbar').addClass(' animateDelay0');
+				this.header.obj.find('.topbar').addClass(' animateDelay05');
 				this.header.obj.find('.fouse-des').addClass(' animateDelay1');
 				this.header.obj.find('.shequ').addClass(' animateDelay2');
 				this.header.obj.find('.mouse').addClass(' animateDelay3');
-
 				this.header = null ;
+
 			}else if( this.blog != null && this.intense( this.blog,wjson ) ){
 
-				this.blog.obj.find('.cat-item').addClass('animated silde');
+				this.blog.obj.find('.cat-item,.page-item').addClass('animated silde');
+				this.blog.obj.find('.page-item').addClass('animateDelay05');
 				this.blog = null ;
 
 			}else if( this.intro != null && this.intense( this.intro,wjson ) ){
 
 				this.intro.obj.find('.intro-show').addClass('animated fadeInLeft');
 				this.intro = null ;
+
+			}else if( this.join != null && this.intense( this.join,wjson ) ){
+
+				this.join.obj.find('h1,p').addClass('animated fadeInDown')
+				this.join.obj.find('p').addClass(' animateDelay1');
+				this.join.obj.find('.join-me').css('-webkit-transform','rotate(0deg)');
+				this.join.obj.find('.type').addClass('animated zoomIn animateDelay2');
+				this.join = null ;
+
 			}
 		},
 		addEvent:function(){
 
 			var This = this;
+			this.initAnimate();
 			$(window).scroll( $.proxy( This.scrollEvent,This ) );
+			for( var i=0,max=this.module.length;i<max;i++){
+
+				this.module.eq(i).on('click',This.runto);
+			}
+			this.btnLogin.on('click',This.loginEvent);
+			this.closeLoin.on('click',This.closeEvent);
 		},
 		scrollEvent:function(){
+			var This = this;
+			this.initAnimate.call(This);
+		},
+		runto:function(event){
+			event.preventDefault();
 
-			var wjson = this.getClient();
+			var data = $(this).data('to'),scrollTop = $(data).offset().top;
 
-			if( this.header != null && this.intense( this.header,wjson ) ){
-
-				this.header.obj.find('.topbar,.fouse-des,.shequ,.mouse').addClass('animated2 fadeInDown')
-				this.header.obj.find('.topbar').addClass(' animateDelay0');
-				this.header.obj.find('.fouse-des').addClass(' animateDelay1');
-				this.header.obj.find('.shequ').addClass(' animateDelay2');
-				this.header.obj.find('.mouse').addClass(' animateDelay3');
-				this.header = null ;
-
-			}else if( this.blog != null && this.intense( this.blog,wjson ) ){
-
-				this.blog.obj.find('.cat-item,.page-item').addClass('animated2 silde');
-				this.blog.obj.find('.page-item').addClass('animateDelay1');
-				this.blog = null ;
-
-			}else if( this.intro != null && this.intense( this.intro,wjson ) ){
-
-				this.intro.obj.find('.intro-show').addClass('animated2 fadeInLeft');
-				this.intro = null ;
-
-			}else if( this.join != null && this.intense( this.join,wjson ) ){
-
-				this.join.obj.find('h1,p').addClass('animated2 fadeInDown')
-				this.join.obj.find('p').addClass(' animateDelay2');
-				this.join.obj.find('.type').addClass('animated2 zoomIn animateDelay3');
-				this.join = null ;
-
-			}
+			$('html,body').animate({
+				'scrollTop':scrollTop
+			},1000,'swing');
+			//$(window).scrollTop(scrollTop);
+		},
+		loginEvent:function(event){
+			event.preventDefault();
+			$('#shadow').addClass('animated05 scaleandrotate');
+		},
+		closeEvent:function(event){
+			event.preventDefault();
+			$('#shadow').removeClass('animated05 scaleandrotate');
 		},
 		getJson:function($p){
 
+			if( $p.length == 0 ){return;}
 			var w,h,t,l;
 			w = $p.width();
-			h = $p.height();
+			h = $p.outerHeight()/2;
 			l = $p.offset().left;
-			t = $p.offset().top;
+			t = $p.offset().top + $p.outerHeight()/2;
 
 			return {obj:$p,width:w,height:h,left:l,top:t};
 		},
@@ -90,7 +99,7 @@
 			l = $(window).scrollLeft();
 			t = $(window).scrollTop();
 			w = $(window).width();
-			h = $(window).height()/4;
+			h = document.body.clientHeight;
 
 			return {width:w,height:h,left:l,top:t};
 		},

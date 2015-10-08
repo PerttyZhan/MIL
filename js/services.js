4154ac7d22166eb9de48fff4a68aae9542419589
he kai <648851130@qq.com>
2015-10-08 09:20:12 +0800

    1

---
+++ js/services.js
@@ -0,0 +1,22 @@
+var editServices = angular.module('editServices', []);
+
+editServices.service('editServices_1', [
+    function() {
+        return [
+            {
+                id:1,
+                title:"标题1"
+            },
+            {
+                id:2,
+                title:"标题2"
+            },
+            {
+                id:3,
+                title:"标题3"
+            }
+        ]
+    }
+]);
+
+
